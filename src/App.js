d3bad86bf45d36e93cf5dc4e0ea15c66ea0921c8
kiran-9951 <arvakirank@gmail.com>
2024-08-01 19:10:46 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./index.css";
 import Navbar from "./components/navbar/navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/home";
 import Cart from "./pages/cart/cart";
 import Footer from "./components/footer/footer";
-import LoginPopup from "./components/loginpopup/loginpopup";
+
 import FoodDetail from "./pages/fooddetail/foodDetail";
 import Menu from "./pages/menu/menu";
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,14 +13,14 @@ import { ToastContainer } from "react-toastify";
 import ItemDetail from "./pages/Itemdetail/itemdetail";
 
 const App = () => {
-  const [showLogin, setShowLogin] = useState(false);
+ 
 
   return (
     <>
     <ToastContainer/>
-      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+ 
       <div className="app">
-        <Navbar setShowLogin={setShowLogin} />
+        <Navbar  />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/food/:id" element={<FoodDetail />} />
